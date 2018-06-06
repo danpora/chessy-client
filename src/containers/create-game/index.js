68337cc52609bfbbb959e50566504cfc29b7d373
danprt <dan.porat@lisuto.com>
2018-06-07 00:02:12 +0300

    Board | add resize event listener

--- src/containers/create-game/index.js
+++ src/containers/create-game/index.js
@@ -6,19 +6,19 @@ import * as actions from '../../actions/createGame';
 import styles from './styles.scss';
 import * as Service from '../../service/socket';
 import { WaitForPeer } from '../../components/create-game/WaitForPeer';
-import { Button } from 'react-bootstrap';
+import { Button, Well, Alert, Panel } from 'react-bootstrap';
 
 class CreateGame extends React.Component {
   componentWillReceiveProps = (nextProps) => {
     if (nextProps.isPeerConnected) {
-      this.props.history.push('/game')      
+      this.props.history.push('/game');
     }
-  }
+  };
   createGame = () => {
     this.props.actions.createGame('white');
   };
 
-  render() {
+  currentElement = () => {
     if (this.props.isFetching) {
       return <Fetching />;
     }
@@ -28,12 +28,18 @@ class CreateGame extends React.Component {
     }
 
     if (this.props.isGameCreated) {
-      return <CreateSuccess gameId={this.props.gameId} recreate={this.createGame}/>;
+      return (
+        <CreateSuccess gameId={this.props.gameId} recreate={this.createGame} />
+      );
     }
 
     return (
       <GameCreationMenu onCreate={this.createGame} onReturn={this.returnHome} />
     );
+  };
+
+  render() {
+    return <div className={styles.centered}>{this.currentElement()}</div>;
   }
 }
 
@@ -43,21 +49,34 @@ const Fetching = () => {
 
 const CreateSuccess = ({ gameId, recreate }) => {
   return (
-    <div>
-      <h3>game created successfully</h3>
-      <h3>gameId:{gameId}</h3>
-      <h3>recreate game</h3>
-      <Button onClick={recreate}>Recreate game</Button>
-    </div>
+    <React.Fragment>
+      <h3>Game created successfully!</h3>
+      <Alert>
+        Share the identifier below with your peer, and wait for him to connect
+      </Alert>
+      <Panel bsStyle={'success'}>
+        <Panel.Heading>Game identifier</Panel.Heading>
+        <Panel.Body>{gameId}</Panel.Body>
+      </Panel>
+      <Button className={styles.button} onClick={recreate}>
+        Recreate
+      </Button>
+    </React.Fragment>
   );
 };
 const GameCreationMenu = (props) => {
   return (
-    <div className={styles.centered}>
+    <React.Fragment>
       <h1>Ready to play?</h1>
       <h2>To create a new game, press the 'Generate game' button</h2>
-      <Button bsStyle={'warning'} className={styles.button} onClick={props.onCreate}>Generate</Button>
-    </div>
+      <Button
+        bsStyle={'warning'}
+        className={styles.button}
+        onClick={props.onCreate}
+      >
+        Generate
+      </Button>
+    </React.Fragment>
   );
 };
 
@@ -81,4 +100,7 @@ function mapDispatchToProps(dispatch) {
   };
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(CreateGame);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(CreateGame);
